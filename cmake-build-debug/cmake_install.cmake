7ec8af2e6036dcaf49fcdae8e2d3f0729dd78d06
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-06-30 15:10:43 +0200

    day2 Updated

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Hady Ahmed/Desktop/Draft/sol/MyChallenge
+# Install script for directory: C:/Users/Hady Ahmed/Documents/GitHub/MyChallenge
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Hady Ahmed/Desktop/Draft/sol/MyChallenge/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Hady Ahmed/Documents/GitHub/MyChallenge/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main() {
 
-    
+
 
     return 0;
 }
