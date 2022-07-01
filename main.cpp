24300b6ecb110fe64eafd4c9445e1ff2f76e96f6
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-01 16:57:54 +0200

    day#3 .2 leetcode

--- main.cpp
+++ main.cpp
@@ -6,46 +6,8 @@
 #include <vector >
 #define ll long long
 using namespace std;
-void Far(){
-    int n ;cin>>n;
-    string a [n];
-    int c =0;
-    for (int i = 0; i < n; ++i) {
-        cin>>a[i];
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if(a[i][j]=='C')
-            {
-                for (int k = j+1; k < n; ++k) {
-                    if(a[i][k]=='C')
-                    {
-                        c++;
-                    }
-                }
-            }
-        }
-    }
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if(a[j][i]=='C')
-            {
-                for (int k = j+1; k < n; ++k) {
-                    if(a[k][i]=='C')
-                    {
-                        c++;
-                    }
-                }
-            }
-        }
-    }
-
-    cout<<c <<endl;
-
-}
 
 int main() {
-    Far();
+
     return 0;
 }
