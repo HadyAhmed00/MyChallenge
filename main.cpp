46621b9b1770683501b0890e14890dc14cd658c2
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-03 09:56:31 +0200

    day#5 leetcode

--- main.cpp
+++ main.cpp
@@ -9,23 +9,6 @@
 #define f(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
 
-void Choosing_Teams(){
-    int n, k ;
-    cin>>n>>k;
-    int c=0;
-
-    for (int i = 0; i < n; ++i) {
-        int input ;
-        cin>>input;
-        int tmp =  5-input;
-        if(tmp>=k)
-            c++;
-    }
-
-    cout<<c/3;
-}
-
 int main() {
-    Choosing_Teams();
-    return 0;
+
 }
