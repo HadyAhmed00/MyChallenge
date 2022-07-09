188c6051842923213599d879b8827a71d6a77a34
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-09 04:27:14 +0200

    day  #11

--- main.cpp
+++ main.cpp
@@ -6,30 +6,14 @@
 #include <vector >
 #include <stack>
 #include <cmath>
+#include <queue>
+
 #define ll long long int
 #define modl 1000000007
 #define reb(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
-double myPow(double x, int n) {
-    if(n==0){
-        return 1;
-    }
-    if(n<0){
-        x = 1/x;
-        n= abs(n);
-    }
-    double res = myPow(x,n/2);
-    double final;
-    if(n%2==0)
-    {
-        final= res*res;
-    }else{
-        final= res*res*x;
-    }
-    return final;
-}
+
 int main(){
 
-    cout<<myPow(2.00000,-2);
     return 0;
 }
