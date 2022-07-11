abb1ed2962fd71fb166d27ad146e1690e5b90a31
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-11 05:09:20 +0200

    day #13

--- day13/rightSideView.cpp
+++ day13/rightSideView.cpp
@@ -1,3 +1,6 @@
+//
+// Created by Hady Ahmed on 7/11/2022.
+//
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -16,14 +19,14 @@
 using namespace std;
 
 // Definition for a binary tree node.
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 vector<int> rightSideView(TreeNode* root) {
 
@@ -46,10 +49,11 @@ vector<int> rightSideView(TreeNode* root) {
     return ans;
 }
 
+//
+//int main(){
+//
+//    improve
+//
+//    return 0;
+//}
 
-int main(){
-
-    improve
-
-    return 0;
-}
