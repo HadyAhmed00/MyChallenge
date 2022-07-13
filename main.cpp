e8112f8f1b3c1316c005826e7f4579667c82f0c3
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-13 04:46:05 +0200

    day #15

--- main.cpp
+++ main.cpp
@@ -15,43 +15,12 @@
 #define reb(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
 
-void Sinking_Ship(){
 
-    map<string, vector<string>> res;
-    int n; cin>>n;
-    for (int i = 0; i < n; ++i) {
-        string val,key;
-        cin>>val>>key;
-        if(key == "child" )
-            key = "woman";
-        if(res.find(key)==res.end())
-        {
-            vector<string> tmp;
-            tmp.push_back(val);
-            res.insert({key,tmp});
-        }else{
-            res[key].push_back(val);
-        }
-    }
-    if(res.find("rat")!=res.end())
-        for(auto it: res["rat"])
-            cout<<it<<endl;
-    if(res.find("woman")!=res.end())
-        for(auto it: res["woman"])
-            cout<<it<<endl;
-    if(res.find("man")!=res.end())
-        for(auto it: res["man"])
-            cout<<it<<endl;
-    if(res.find("captain")!=res.end())
-        for(auto it: res["captain"])
-            cout<<it<<endl;
-
-}
 
 
 int main(){
 
     improve
-    Sinking_Ship();
+
     return 0;
 }
