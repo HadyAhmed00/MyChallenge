1b814a882ead3223e8dea5b795b149c819c78ded
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-17 16:18:46 +0200

    day #19

--- day19/+ONE.cpp
+++ day19/+ONE.cpp
@@ -30,34 +30,35 @@
 #define END cout<<endl;
 #define reb(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
-void Toy_Cars(){
-    vector<int> res;
-    int n;cin>>n;
-    for (int i = 0; i < n; ++i) {
-        bool is_good = true;
-        for (int j = 0; j < n; ++j) {
-            int tmp;
-            cin>>tmp;
-            if(tmp==1||tmp==3)
-            {
-                is_good= false;
-
-            }
-
-        }
-        if(is_good) res.push_back(i+1);
+vector<int> plusOne(vector<int>& digits) {
+    if(digits[digits.size()-1]!=9){
+        digits[digits.size()-1]+=1;
+        return digits;
     }
-    cout<<res.size();
-    END
-    for(auto it: res)
-        cout<<it<<" ";
+    int i = digits.size()-1;
+    while (digits[i]==9&&i>0){
+        digits[i]=0;
+        i--;
+    }
 
+    if(digits[i]==9)
+    {
+        digits[i]=0;
+        digits.insert(digits.begin(),1);
+    }
+    else{
+        digits[i]+=1;
+    }
+    for(auto it:digits) cout<<it<<endl;
+    END
+    return digits;
 
 }
 //int main(){
 //
 //    improve
-//    Toy_Cars();
+//    vector<int> v = {8,9,9,9};
+//    plusOne(v);
 //    END
 //    return 0;
 //}
--- day19/deleteDuplicatesLinkedList.cpp
+++ day19/deleteDuplicatesLinkedList.cpp
@@ -21,43 +21,40 @@
 #include <queue>
 #include <utility>
 #include <cstring>
-
 #define improve ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
 #define all(v) v.begin(), v.end()
 #define loop(n) for (int i = 0; i < n; i++)
 #define ll long long int
-
 #define END cout<<endl;
 #define reb(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
-void Toy_Cars(){
-    vector<int> res;
-    int n;cin>>n;
-    for (int i = 0; i < n; ++i) {
-        bool is_good = true;
-        for (int j = 0; j < n; ++j) {
-            int tmp;
-            cin>>tmp;
-            if(tmp==1||tmp==3)
-            {
-                is_good= false;
 
-            }
-
+/**
+ * Definition for singly-linked list.*/
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode* deleteDuplicates(ListNode* head) {
+    ListNode *curr=head;
+    while(curr!=NULL&&curr->next!=NULL){
+        if(curr->val==curr->next->val)
+        {
+            curr->next=curr->next->next;
         }
-        if(is_good) res.push_back(i+1);
+        else
+            curr=curr->next;
     }
-    cout<<res.size();
-    END
-    for(auto it: res)
-        cout<<it<<" ";
-
-
+    return head;
 }
+//
 //int main(){
 //
 //    improve
-//    Toy_Cars();
+//
 //    END
 //    return 0;
 //}
