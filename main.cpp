2c1178f244f67d7c81b9253517ab645e8c70d0b0
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-19 09:35:26 +0200

    day #20

--- main.cpp
+++ main.cpp
@@ -25,36 +25,6 @@
 #define reb(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
 
-vector<vector<int>> generate(int numRows) {
-    vector<vector<int>>res;
-    for (int i = 1; i <=numRows ; ++i) {
-        vector<int > row;
-
-        for (int j = 0; j < i; ++j) {
-            if(j==0){
-                row.push_back(1);
-            }else if(j==i-1)
-            {
-                row.push_back(1);
-            }
-            else{
-                row.push_back(res[i-2][j-1]+res[i-2][j]);
-            }
-        }
-
-        res.push_back(row);
-
-    }
-//    for(auto it : res){
-//        for(auto num: it){
-//            cout<<num<<" ";
-//        }
-//        cout<<endl;
-//    }
-    return res;
-
-
-}
 int main(){
 
     improve
