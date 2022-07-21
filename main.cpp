9c995b8ccd0d598b7b9c1e9d4445cded1b61c7e4
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-21 18:33:02 +0200

    day #22

--- main.cpp
+++ main.cpp
@@ -24,12 +24,35 @@
 #define END cout<<endl;
 #define reb(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
+void IQ(){
+    int a[1001];
+    int n,i,num1=0,num2=0,cnt2=0,cnt1=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        if(a[i]%2==0)
+        {
+            cnt1++;
+            num1=i;
+        }
+        else
+        {
+            cnt2++;
+            num2=i;
+        }
+    }
+
+    if(cnt1==1)
+        cout<<num1+1<<endl;
+    if(cnt2==1)
+        cout<<num2+1<<endl;
+}
 
 int main(){
 
     improve
-
-
+    IQ();
     END
     return 0;
 }
