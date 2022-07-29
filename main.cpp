f678cd45190a310672bb2a97c6777c866f8da327
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-07-29 07:09:27 +0200

    day #30

--- main.cpp
+++ main.cpp
@@ -25,50 +25,9 @@
 #define reb(i, k, n) for (ll i = k; i < n; i++)
 using namespace std;
 
-int lengthOfLongestSubstring(string s) {
-    int Max = INT_MIN;
-    set<char> lookUp;
-    int set_size=0;
-    for(auto it:s){
-        lookUp.insert(it);
-        if(set_size==lookUp.size()){
-            Max = max(Max,set_size);
-            lookUp.clear();
-            set_size=0;
-            lookUp.insert(it);
-        }
-        else{
-            set_size=lookUp.size();
-        }
-    }
-    return max(Max,set_size);
-
-}
-//slikdeing window approche
-
-int lengthOfLongestSubstring1(string s){
-    map<char,int>mp;
-    int i=0,j=0,res=0;
-    while(j<s.length()){
-        mp[s[j]]++;
-        if(mp.size()<j-i+1){
-            while(!mp.empty() && mp.size()<j-i+1){
-                mp[s[i]]--;
-                if(mp[s[i]]==0) mp.erase(s[i]);
-                i++;
-            }
-            j++;
-        }else if(mp.size()==j-i+1){
-            res=max(res,j-i+1);
-            j++;
-        }
-    }
-    return res;
-}
 int main(){
     improve
-    string s="dvdf";
-    cout<<lengthOfLongestSubstring(s);
+
     END
     return 0;
 }
