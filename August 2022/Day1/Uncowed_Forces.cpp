f757cf6e33c7b267afe60d6349f041296486f519
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-08-01 05:30:21 +0200

    Day #33

---
+++ August 2022/Day1/Uncowed_Forces.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Hady Ahmed on 8/1/2022.
+//
+/*
+ █████╗ ██╗   ██╗ ██████╗ ██╗   ██╗███████╗████████╗       ██████╗  █████╗ ██╗   ██╗     ██╗ ██╗  ██╗
+██╔══██╗██║   ██║██╔════╝ ██║   ██║██╔════╝╚══██╔══╝       ██╔══██╗██╔══██╗╚██╗ ██╔╝    ████████╗███║
+███████║██║   ██║██║  ███╗██║   ██║███████╗   ██║          ██║  ██║███████║ ╚████╔╝     ╚██╔═██╔╝╚██║
+██╔══██║██║   ██║██║   ██║██║   ██║╚════██║   ██║          ██║  ██║██╔══██║  ╚██╔╝      ████████╗ ██║
+██║  ██║╚██████╔╝╚██████╔╝╚██████╔╝███████║   ██║          ██████╔╝██║  ██║   ██║       ╚██╔═██╔╝ ██║
+╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝          ╚═════╝ ╚═╝  ╚═╝   ╚═╝        ╚═╝ ╚═╝  ╚═╝
+
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <vector >
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <utility>
+#include <cstring>
+#define improve ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define loop(n) for (int i = 0; i < n; i++)
+#define ll long long int
+#define END cout<<endl;
+#define reb(i, k, n) for (ll i = k; i < n; i++)
+using namespace std;
+int solve(int i, int j, vector<vector<int>> &dp, int& m, int& n) {
+    if(i == m-1 || j == n-1) return 1;
+    if(dp[i][j] != 0)return dp[i][j];
+
+    dp[i][j] = solve(i+1, j, dp, m, n) + solve(i, j+1, dp, m, n);
+    return dp[i][j];
+}
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m , vector<int>(n , 0));
+    return solve(0, 0, dp, m, n);
+}
+int main(){
+    improve
+    END
+}
---
+++ August 2022/Day1/uniquePath.cpp
@@ -0,0 +1,59 @@
+//
+// Created by Hady Ahmed on 8/1/2022.
+//
+/*
+ █████╗ ██╗   ██╗ ██████╗ ██╗   ██╗███████╗████████╗       ██████╗  █████╗ ██╗   ██╗     ██╗ ██╗  ██╗
+██╔══██╗██║   ██║██╔════╝ ██║   ██║██╔════╝╚══██╔══╝       ██╔══██╗██╔══██╗╚██╗ ██╔╝    ████████╗███║
+███████║██║   ██║██║  ███╗██║   ██║███████╗   ██║          ██║  ██║███████║ ╚████╔╝     ╚██╔═██╔╝╚██║
+██╔══██║██║   ██║██║   ██║██║   ██║╚════██║   ██║          ██║  ██║██╔══██║  ╚██╔╝      ████████╗ ██║
+██║  ██║╚██████╔╝╚██████╔╝╚██████╔╝███████║   ██║          ██████╔╝██║  ██║   ██║       ╚██╔═██╔╝ ██║
+╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝          ╚═════╝ ╚═╝  ╚═╝   ╚═╝        ╚═╝ ╚═╝  ╚═╝
+
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <vector >
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <utility>
+#include <cstring>
+#define improve ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define loop(n) for (int i = 0; i < n; i++)
+#define ll long long int
+#define END cout<<endl;
+#define reb(i, k, n) for (ll i = k; i < n; i++)
+using namespace std;
+void Uncowed_Forces(){
+    float points[]={500,1000,1500,2000,2500};
+    float min[6]={};
+    float w[6]={};
+    float good,bad;
+    for (int i = 0; i < 5; ++i) {
+        cin>>min[i];
+    }
+    for (int i = 0; i < 5; ++i) {
+        cin>>w[i];
+    }
+    cin>>good>>bad;
+    float total=0;
+    for (int i = 0; i < 5; ++i) {
+        float first = 0.3*points[i];
+        float second = ( (1-(min[i]/250))*(points[i]))-(50*w[i]);
+        total+=max(first,second);
+    }
+    total+=(100*good);
+    total-=(50*bad);
+    cout<<total<<endl;
+
+
+}
+int main(){
+    improve
+    Uncowed_Forces();
+    END
+}
